9d6d920baaa55b701c3c0e0c93634df1c25dfb5e
Rafael Kitoco Pambo <rkitoco@gmail.com>
2023-12-14 16:38:42 +0100

    Adicao do tempo e fps

--- HandsMarks/handsmarkBasics.py
+++ HandsMarks/handsmarkBasics.py
@@ -1,10 +1,14 @@
 import mediapipe as mp
 import cv2
+import time
 
 cap = cv2.VideoCapture(0)
 handsMarks = mp.solutions.hands
 hands = handsMarks.Hands()
 handsdraw = mp.solutions.drawing_utils
+
+ctime = 0
+ptime = 0
 
 while True:
     sucess, img = cap.read()
@@ -14,6 +18,11 @@ while True:
     if result.multi_hand_landmarks:
         for hand in result.multi_hand_landmarks:
             handsdraw.draw_landmarks(img, hand)
+    
+    ctime = time.time()
+    fps = 1/(ctime - ptime)
+    ptime = ctime
 
+    cv2.putText(img, str(int(fps)), (20, 70), cv2.FONT_HERSHEY_PLAIN, 5, (255, 0, 255), 3)
     cv2.imshow("img", img)
     cv2.waitKey(1)
