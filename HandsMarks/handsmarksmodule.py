cbef9b1295cc0083648150f7c4c2e8219ea51b09
Rafael Kitoco Pambo <rkitoco@gmail.com>
2023-12-14 17:19:38 +0100

    Adicao do biblioteca de detecao de maos

---
+++ HandsMarks/handsmarksmodule.py
@@ -0,0 +1,41 @@
+import mediapipe as mp
+import cv2
+import time
+
+class HandmarksDetection():
+    def __init__(self):
+        self.handsMarks = mp.solutions.hands
+        self.hands = self.handsMarks.Hands()
+        self.handsdraw = mp.solutions.drawing_utils
+    
+    def findhands(self, img):
+        imageCV2 = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        result = self.hands.process(imageCV2)
+
+        if result.multi_hand_landmarks:
+            for hand in result.multi_hand_landmarks:
+                for id, ln in enumerate(hand.landmark):
+                    h, w, c = img.shape
+                    cx, cy = int((ln.x*w)), int((ln.y*h))
+                    cv2.circle(img,(cx, cy), 5, (255,0,255), cv2.FILLED)
+                self.handsdraw.draw_landmarks(img, hand, self.handsMarks.HAND_CONNECTIONS)
+        return img
+
+def main():
+    cap = cv2.VideoCapture(0)
+    ctime = 0
+    ptime = 0
+    detected = HandmarksDetection()
+    while True:
+        sucess, img = cap.read()
+        img = detected.findhands(img)
+        ctime = time.time()
+        fps = 1/(ctime - ptime)
+        ptime = ctime
+
+        cv2.putText(img, str(int(fps)), (20, 70), cv2.FONT_HERSHEY_PLAIN, 5, (255, 0, 255), 3)
+        cv2.imshow("img", img)
+        cv2.waitKey(1)
+
+if __name__ == "__main__":
+    main()
